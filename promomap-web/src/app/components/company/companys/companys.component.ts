d2622d3dddd9dc818e8d4d733a52b66157649e56
Leandro Santos <leandro.lucas_@hotmail.com>
2017-05-14 18:38:43 -0300

    [WEB] user profile*

---
+++ promomap-web/src/app/components/company/companys/companys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companys',
+  templateUrl: './companys.component.html',
+  styleUrls: ['./companys.component.css']
+})
+export class CompanysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
